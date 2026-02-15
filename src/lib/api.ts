4703150a116818ee4621e151bfe761e08c9ed49d
HawkingHuang <math0915@gmail.com>
2026-02-16 00:37:05 +0800

    chore: reduce images downloaded size

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL = "https://api.themoviedb.org/3";
 
-// 780 * 439 backdrops
-export const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w780";
+// 500 * 281 backdrops
+export const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 // 500 * 750 movie posters
 export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500";
